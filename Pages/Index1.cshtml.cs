c38a9dbec6133d4fd8210414a53bfc14f9d9cfcc
Jean <jean.alvarez@al.infnet.edu.br>
2024-02-20 18:39:06 -0300

    Funcao de saudacao

---
+++ Pages/Index1.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Armazem_dotnet.Pages
+{
+    public class Index1Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- obj/Debug/net8.0/Armazem_dotnet.AssemblyInfo.cs
+++ obj/Debug/net8.0/Armazem_dotnet.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Armazem_dotnet")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f8aa363f2c3dc7e1fc9f3061d7da6746968d140")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fcd970e544334803810c74a17669a1ff934356ac")]
 [assembly: System.Reflection.AssemblyProductAttribute("Armazem_dotnet")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Armazem_dotnet")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
